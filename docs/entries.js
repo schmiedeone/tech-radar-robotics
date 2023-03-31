6fed9555e28aa0ac0c04b7b4230f2df02a697166
Ainuddin Faizan <andyfaizan@gmail.com>
2023-03-31 18:12:53 +0200

    feat(entries): fetch entries from buttercms

--- docs/entries.js
+++ docs/entries.js
@@ -1,6 +1,16 @@
 function getEntries() {
   const entries = fetch(
-    "https://mocki.io/v1/f28f1c01-081c-480e-893a-b804f6dc8cd4"
-  ).then((response) => response.json());
+    "https://api.buttercms.com/v2/content/dcj-tech-radar/?locale=de&auth_token=f07ae6384c65da8195f7d48d0bc2db5e7948fe0d"
+  ).then((response) => {
+    return response.json().then((data) => {
+      const entries = data.data["dcj-tech-radar"];
+      return entries.map((entry) => {
+        const moddedEntry = Object.assign({}, entry);
+        moddedEntry.ring = Number(entry.ring);
+        moddedEntry.quadrant = Number(entry.quadrant);
+        return moddedEntry;
+      });
+    });
+  });
   return entries;
 }
